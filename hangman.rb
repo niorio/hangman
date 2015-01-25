00bbc57743ba63aa2cfc98094ef075fd0e6d2a5c
Nicholas Iorio <niorio1@gmail.com>
2015-01-25 17:29:01 -0500

    added humanplayer class

--- hangman.rb
+++ hangman.rb
@@ -21,11 +21,11 @@ class Hangman
         update_board(guess, response)
       end
 
-      @guesser.hand_response(guess, response)
+      @guesser.handle_response(guess, response)
 
     end
 
-    won? ? puts "Guesser Wins!" : puts "Guesser Loses!"
+    won? ? puts("Guesser Wins!") : puts("Guesser Loses!")
 
     nil
   end
@@ -41,6 +41,36 @@ class Hangman
 end
 
 
+class HumanPlayer
+
+  def register_secret_length(secret_length)
+
+    puts "The word is #{secret_length} letters long: "
+  end
+
+  def get_guess(board, num_remaining_guesses)
+    puts "You have #{num_remaining_guesses} guesses left"
+    board.each {|letter| letter.nil? ? print("_") : print(letter)}
+
+    print "Your guess: "
+    gets.chomp
+  end
+
+  def check_guess(guess)
+    puts "Opponent guessed #{guess}"
+    puts "What positions does this letter occure at? (if none press enter)"
+
+    positions = gets.chomp.split(",")
+    positions.map! {|pos| pos.to_i}
+    positions
+
+  end
+
+  def handle_response(guess, response)
+    #only the computer needs this method.  a human can see what's on screen.
+  end
+
+end
 
 
 
